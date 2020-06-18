40403f79df4c301d64bf3ee5184b7892d9d20f1c
dasamith <60905198+dasamith@users.noreply.github.com>
2020-06-18 16:02:38 -0400

    update  exercise and stats  function

--- server.js
+++ server.js
@@ -28,6 +28,20 @@ app.get('/exercise', function (req, res) {
     res.sendFile(path.join(__dirname, '/public', 'exercise.html'));
 })
 
+app.get('/stats', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'stats.html'));
+})
+
+app.get('/api/workouts/range', function (request, response) {
+    console.log('we smacked the rnage route!! time to get ppl out of database!!')
+    workoutmodel.find({}).limit(10).then(function (pplFromDB) {
+        response.json(pplFromDB)
+    })
+})
+
+
+
+
 app.post('/api/workouts', function (req, res) {
 
     console.log("we hit the workput route")
@@ -43,7 +57,18 @@ app.put('/api/workouts/:id', function (req, res) {
     // workoutmodel.create({}).then(function (newWorkoutWeMade) {
     //     res.json(newWorkoutWeMade)
     // })
+    var newExercise = req.body
+    // workoutmodel.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: newExercise } }).then(function (data, err) {
+    //     console.log('this is the data and err', data, err)
+    // })
 
+    workoutmodel.update(
+        { _id: req.params.id },
+        { $push: { exercises: newExercise } },
+        function (data, err) {
+            console.log('this is the data and err', data, err)
+        }
+    );
 
 })
 
